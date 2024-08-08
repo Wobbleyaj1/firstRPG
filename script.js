4cfaa5655afbba85b4e9e46c41744f704d4b3384
jnipper7 <jay.nipper@gtri.gatech.edu>
2024-08-08 16:58:44 -0400

    Implemented entities and added a fight sequence.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let xp = 0;
-let gold = 50;
 let fighting;
 let monsterHealth;
+
+const healAmount = 10;
+const healthPrice = 10;
+const maxHealth = 100;
 
 const button1 = document.querySelector('#button1');
 const button2 = document.querySelector('#button2');
@@ -14,13 +16,64 @@ const monsterStats = document.querySelector('#monsterStats');
 const monsterNameText = document.querySelector('#monsterNameText');
 const monsterHealthText = document.querySelector('#monsterHealthText');
 
-//Health variables
-const healAmount = 10;
-const healthPrice = 10;
-const maxHealth = 100;
-let health = 100;
+//Map locations
+const locations = [
+    {
+        name: "town square",
+        "button text": ['Go to store', 'Go to cave', 'fightDragon'],
+        "button functions": [goStore, goCave, fightDragon],
+        text: 'You are in the town square. You see a sign that says \"store\".'
+    },
+    {
+        name: "store",
+        "button text": [`Buy ${healAmount} health (${healthPrice} gold)`, 'Upgrade weapon (30 gold)', 'Go to town square'],
+        "button functions": [buyHealth, upgradeWeapon, goTown],
+        text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ['Fight slime', 'Fight beast', 'Go to town square'],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ['Attack', 'Dodge', 'Run'],
+        "button functions": [attack, dodge, goTown],
+        text: 'You encounter a monster.'
+    }
+];
 
-//Weapon variables
+//Entities
+let human = {
+        xp: 1,
+        gold: 50,
+        health: 100,
+        damage: 0
+    }
+
+const monsters = [
+    {
+        name: "Slime",
+        health: 25,
+        level: 1,
+        damage: 5
+    },
+    {
+        name: "Beast",
+        health: 100,
+        level: 10,
+        damage: 15
+    },
+    {
+        name: "Dragon",
+        health: 1500,
+        level: 100,
+        damage: 45
+    }
+];
+
+//Weapons
 const weapons = [
     {
         name: "Stick",
@@ -44,30 +97,9 @@ const weapons = [
     }
 ];
 let currentWeapon = 0;
-let maxWeapon = 3;
+let maxWeapon = weapons.length - 1;
 
-const locations = [
-    {
-        name: "town square",
-        "button text": ['Go to store', 'Go to cave', 'fightDragon'],
-        "button functions": [goStore, goCave, fightDragon],
-        text: 'You are in the town square. You see a sign that says \"store\".'
-    },
-    {
-        name: "store",
-        "button text": [`Buy ${healAmount} health (${healthPrice} gold)`, 'Upgrade weapon (30 gold)', 'Go to town square'],
-        "button functions": [buyHealth, upgradeWeapon, goTown],
-        text: "You enter the store."
-    },
-    {
-        name: "cave",
-        "button text": ['Fight slime', 'Fight fanged beast', 'Go to town square'],
-        "button functions": [fightSlime, fightBeast, goTown],
-        text: "You enter the cave. You see some monsters."
-    }
-];
-
-//initialize buttons
+// #region initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
@@ -81,6 +113,9 @@ function update(location) {
     button3.onclick = location["button functions"][2];
     text.innerText = location.text;
 }
+// #endregion
+
+// #region Warp functions
 function goTown() {
     update(locations[0])
 }
@@ -93,38 +128,90 @@ function goCave() {
     update(locations[2])
 }
 
+function goFight(){
+    update(locations[3])
+}
+// #endregion
+
+// #region Fight functions
 function fightSlime() {
-    console.log("Fighting slime.")
+    fighting = 0;
+    prepFight();
 }
 
 function fightBeast() {
-    console.log("Fighting beast.")
+    fighting = 1;
+    prepFight();
 }
 
 function fightDragon() {
-    console.log("Fighting dragon.")
+    fighting = 2;
+    prepFight();
 }
 
-function buyHealth() {
-    if (gold >= healthPrice && health <= maxHealth - healAmount)
+function prepFight() {
+    goFight();
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterNameText.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+    human.damage = (1 + Math.log(human.xp)) * weapons[currentWeapon].power;
+}
+
+function attack() {
+    text.innerText = `You attack it with your ${weapons[currentWeapon].name} and deal ${human.damage} damage.`;
+    monsterHealth -= human.damage;
+    if (monsterHealth <= 0)
     {
-        gold -= healthPrice;
-        health += healAmount;
+        monsterHealthText.innerText = 0;
+        human.xp += monsters[fighting].level;
+        human.gold += (monsters[fighting].level * (getRandomInt(5)));
+        defeatMonster();
     }
-    else if (health = maxHealth)
+    else
+    {
+        monsterHealthText.innerText = monsterHealth;
+    }
+    text.innerText += `\nThe ${monsters[fighting].name} attacks.`;
+    human.health -= monsters[fighting].damage;
+    if (human.health <= 0)
+    {
+        healthText.innerText = 0;
+        gameOver();
+    }
+    else
+    {
+        healthText.innerText = human.health;
+    }
+
+}
+
+function dodge() {
+    
+}
+// #endregion
+
+// #region Shop functions
+function buyHealth() {
+    if (human.gold >= healthPrice && human.health <= maxHealth - healAmount)
+    {
+        human.gold -= healthPrice;
+        human.health += healAmount;
+    }
+    else if (human.health = maxHealth)
     {
         text.innerText = "You have reached the health limit."
     }
-    else if (gold < healthPrice)
+    else if (human.gold < healthPrice)
     {
         text.innerText = "You have insufficient funds."
     }
-    else if (health < maxHealth)
+    else if (human.health < maxHealth)
     {
-        health = maxHealth
+        human.health = maxHealth
     }
-    goldText.innerText = gold;
-    healthText.innerText = health;
+    goldText.innerText = human.gold;
+    healthText.innerText = human.health;
 }
 
 function upgradeWeapon() {
@@ -132,7 +219,7 @@ function upgradeWeapon() {
     {
         text.innerText = "You already have the best weapon in the lands."
     }
-    else if (gold < weapons[currentWeapon + 1].price)
+    else if (human.gold < weapons[currentWeapon + 1].price)
     {
         text.innerText = "You have insufficient funds."
     }
@@ -140,7 +227,14 @@ function upgradeWeapon() {
     {
         currentWeapon++;
         text.innerText = `You have upgraded your weapon to a ${weapons[currentWeapon].name}`;
-        gold -= weapons[currentWeapon].price;
-        goldText.innerText = gold;
+        human.gold -= weapons[currentWeapon].price;
+        goldText.innerText = human.gold;
     }
 }
+// #endregion
+
+// #region Helper functions
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * (max - 1)) + 1;
+    }
+// #endregion
